fa740372f4a93f0fe5dc128d65f21411b854ca4f
Aedan Cullen <aedan@aedancullen.com>
2024-09-04 00:40:48 -0700

    JIT dwf_mipidsi_lpspy.py, handle LP-11 for multiple xfers per file

--- dwf_mipidsi_lpspy.py
+++ dwf_mipidsi_lpspy.py
@@ -1,42 +1,55 @@
-# Decode MIPI DSI LP-mode commands on lane 0 from a set of scope captures
+# Decode MIPI DSI LP-mode commands on lane 0 from a set of scope or logic captures
 
 import sys
 import numpy as np
+from numba import njit
 
 THRESH = 0.7
 
-for filename in sys.argv[1:]:
-    print(filename)
-    with open(filename, 'r') as fh:
-        lines = [line for line in fh if not line.startswith('#') and not line.startswith('\n')]
-        data = np.loadtxt(lines, delimiter=',', skiprows=1)
+@njit
+def process_one_dataset(data):
     last_p = 0
     last_n = 0
     reg = 0b0
-    count = None
+    count = -1
+    out = []
     for i in range(data.shape[0]):
-        p = data[i][1]
-        n = data[i][2]
+        p = data[i, 0]
+        n = data[i, 1]
         if p > THRESH and last_p < THRESH:
-            if count is not None:
+            if count != -1:
                 count += 1
             reg |= (0b1 << 8)
             reg >>= 1
         elif n > THRESH and last_n < THRESH:
-            if count is not None:
+            if count != -1:
                 count += 1
             reg >>= 1
 
-        if count is None and reg == 0b10000111:
+        if count == -1 and reg == 0b10000111:
             count = 0
 
         if count == 8:
             count = 0
-            print(hex(reg), end=" ")
+            out.append(reg)
             reg = 0b0
+
+        if count != -1 and p > THRESH and n > THRESH:
+            count = -1
+            out.append(-1)
 
         last_p = p
         last_n = n
 
-    if count is not None:
-        print()
+    return out
+
+for filename in sys.argv[1:]:
+    print("Loading", filename)
+    data = np.loadtxt(filename, delimiter=',', dtype=np.float32, usecols=range(1,3), skiprows=10)
+    print("Processing", filename)
+    out = process_one_dataset(data)
+    for item in out:
+        if item != -1:
+            print(hex(item), end=" ")
+        else:
+            print()
